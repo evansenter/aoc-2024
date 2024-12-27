c7a9e8088950fec80aebf233b50943e0743cdad6
Evan Senter <evansenter@gmail.com>
2024-12-27 23:34:13 +0000

    5.2 works.

--- src/solutions/day05.rs
+++ src/solutions/day05.rs
@@ -1,9 +1,9 @@
 use crate::utils::input;
 use itertools::Itertools;
-use std::collections::{BTreeMap, HashMap, HashSet};
+use std::collections::{HashMap, HashSet};
 
 pub fn solve() {
-    let input = input::read_file("inputs/day05_test.txt");
+    let input = input::read_file("inputs/day05.txt");
 
     let [transitions, page_lists] = input.split("\n\n").collect::<Vec<&str>>()[..] else {
         panic!("Invalid input");
@@ -20,7 +20,10 @@ pub fn solve() {
         .collect();
 
     println!("Part 1: {}", solve_part1(&transitions, &page_lists));
-    println!("Part 2: {}", solve_part2(&transitions, page_lists.as_mut_slice()));
+    println!(
+        "Part 2: {}",
+        solve_part2(&transitions, page_lists.as_mut_slice())
+    );
 }
 
 fn build_transition_maps(
@@ -40,116 +43,87 @@ fn build_transition_maps(
     (forward, backward)
 }
 
-fn sum_page_list_centers(
+fn invalid_page_list_indices(
     page_lists: &[Vec<i32>],
-    invalid_page_list_indices: &HashSet<usize>,
-) -> i32 {
+    good_transitions: &HashMap<i32, HashSet<i32>>,
+    bad_transitions: &HashMap<i32, HashSet<i32>>,
+) -> HashSet<usize> {
     page_lists
         .iter()
         .enumerate()
         .filter_map(|(i, page_list)| {
-            if invalid_page_list_indices.contains(&i) {
-                None
+            if !check_page_list_validity(page_list, good_transitions, bad_transitions) {
+                Some(i)
             } else {
-                page_list.get((page_list.len() - 1) / 2)
+                None
             }
         })
-        .sum()
+        .collect()
 }
 
-fn solve_part1(transitions: &Vec<Vec<i32>>, page_lists: &[Vec<i32>]) -> i32 {
-    let (good_transitions, bad_transitions) = build_transition_maps(&transitions);
+fn check_page_list_validity(
+    page_list: &[i32],
+    good_transitions: &HashMap<i32, HashSet<i32>>,
+    bad_transitions: &HashMap<i32, HashSet<i32>>,
+) -> bool {
+    !page_list.iter().combinations(2).any(|v| {
+        let [a, b] = v[..] else { unreachable!() };
 
-    let mut invalid_page_list_indices: HashSet<usize> = HashSet::new();
-
-    for (i, page_list) in page_lists.iter().enumerate() {
-        page_list.iter().combinations(2).for_each(|v| {
-            let [a, b] = v[..] else {
-                unreachable!("Should never get here - combinations(2) always returns pairs")
-            };
-
-            if let Some(bad_transitions_for_a) = bad_transitions.get(a) {
-                if bad_transitions_for_a.contains(b) {
-                    invalid_page_list_indices.insert(i);
-                }
-            } else if !good_transitions.get(a).unwrap().contains(b) {
-                unreachable!("{} to {} not found", a, b);
-            }
-        });
-    }
-
-    log::debug!("Invalid page list indices: {:?}", invalid_page_list_indices);
-
-    sum_page_list_centers(page_lists, &invalid_page_list_indices)
+        if let Some(bad_transitions_for_a) = bad_transitions.get(a) {
+            bad_transitions_for_a.contains(b)
+        } else if good_transitions.get(a).unwrap().contains(b) {
+            false
+        } else {
+            unreachable!("{} to {} not found", a, b)
+        }
+    })
 }
 
-fn solve_part2(transitions: &Vec<Vec<i32>>, page_lists: &mut [Vec<i32>]) -> i32 {
+fn sum_page_list_centers(page_lists: &[Vec<i32>], pl_indices: &[usize]) -> i32 {
+    pl_indices.iter().fold(0, |acc, i| {
+        acc + page_lists[*i][(page_lists[*i].len() - 1) / 2]
+    })
+}
+
+fn solve_part1(transitions: &[Vec<i32>], page_lists: &[Vec<i32>]) -> i32 {
     let (good_transitions, bad_transitions) = build_transition_maps(&transitions);
 
-    let mut invalid_pages_by_prefix_by_index: HashMap<usize, BTreeMap<i32, Vec<i32>>> =
-        HashMap::new();
+    let invalid_pl_indices =
+        invalid_page_list_indices(page_lists, &good_transitions, &bad_transitions);
 
-    for (i, page_list) in page_lists.iter().enumerate() {
-        let mut printed_header: bool = false;
+    let valid_pl_indices = (0..page_lists.len())
+        .collect::<HashSet<usize>>()
+        .difference(&invalid_pl_indices)
+        .cloned()
+        .collect::<Vec<usize>>();
 
-        page_list.iter().combinations(2).for_each(|v| {
-            let [a, b] = v[..] else {
-                unreachable!("Should never get here - combinations(2) always returns pairs")
-            };
+    sum_page_list_centers(page_lists, &valid_pl_indices)
+}
 
-            if let Some(bad_transitions_for_a) = bad_transitions.get(a) {
-                if bad_transitions_for_a.contains(b) {
-                    invalid_pages_by_prefix_by_index
-                        .entry(i)
-                        .or_default()
-                        .entry(*a)
-                        .or_default()
-                        .push(*b);
+fn solve_part2(transitions: &[Vec<i32>], page_lists: &mut [Vec<i32>]) -> i32 {
+    let (good_transitions, bad_transitions) = build_transition_maps(transitions);
 
-                    if !printed_header {
-                        log::debug!("Invalid page list: {:?}", page_list);
-                        log::debug!("Good transitions: {:?}", good_transitions);
-                        log::debug!("Bad transitions: {:?}", bad_transitions);
-                        printed_header = true;
-                    }
+    let invalid_pl_indices =
+        invalid_page_list_indices(page_lists, &good_transitions, &bad_transitions);
 
-                    log::debug!("{} to {} is invalid", a, b);
+    for i in invalid_pl_indices.iter() {
+        let page_list = &mut page_lists[*i];
+
+        for v in (0..page_list.len()).combinations(2) {
+            let [j, k] = v[..] else { unreachable!() };
+
+            if let Some(bad_transitions_for_a) = bad_transitions.get(&page_list[j]) {
+                if bad_transitions_for_a.contains(&page_list[k]) {
+                    page_list.swap(j, k);
                 }
-            } else if !good_transitions.get(a).unwrap().contains(b) {
-                unreachable!("{} to {} not found", a, b);
             }
-        });
+        }
     }
 
-    log::debug!("Invalid page transitions: {:?}", invalid_pages_by_prefix_by_index);
-    
-    for (i, invalid_pages_by_prefix) in invalid_pages_by_prefix_by_index.into_iter() {
-        let page_list = &mut page_lists[i];
-
-        log::debug!("Page list {} before update: {:?}", i, page_list);
-
-        invalid_pages_by_prefix.into_iter().rev().for_each(|(prefix, invalid_pages)| {
-            log::debug!("Prefix: {:?}, invalid pages: {:?}", prefix, invalid_pages);
-
-            let prefix_index = page_list.iter().position(|&page| page == prefix).unwrap();
-            log::debug!("Prefix index: {:?}", prefix_index);
-
-            let Some(max_invalid_page_index) = invalid_pages.iter().map(|&invalid_page| {
-                page_list.iter().position(|&page| page == invalid_page).unwrap()
-            }).max() else {
-                unreachable!("No invalid pages found");
-            };
-
-            log::debug!("Max invalid page: {:?}", max_invalid_page_index);
-
-            page_list.remove(prefix_index);
-            page_list.insert(max_invalid_page_index, prefix);
-
-            log::debug!("Page list {} after update: {:?}", i, page_list);
-        });
-    }
-
-    solve_part1(transitions, page_lists)
+    sum_page_list_centers(
+        page_lists,
+        &invalid_pl_indices.into_iter().collect::<Vec<_>>(),
+    )
 }
 
 #[cfg(test)]
