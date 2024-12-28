693112efb6026ba8dabd351f0b11868d717e1574
Evan Senter <evansenter@gmail.com>
2024-12-28 00:10:01 +0000

    5.2 cleanup

--- src/solutions/day05.rs
+++ src/solutions/day05.rs
@@ -2,128 +2,122 @@ use crate::utils::input;
 use itertools::Itertools;
 use std::collections::{HashMap, HashSet};
 
-pub fn solve() {
-    let input = input::read_file("inputs/day05.txt");
+type TransitionMap = HashMap<i32, HashSet<i32>>;
+type PageList = Vec<i32>;
 
-    let [transitions, page_lists] = input.split("\n\n").collect::<Vec<&str>>()[..] else {
-        panic!("Invalid input");
-    };
-
-    let transitions: Vec<Vec<i32>> = transitions
-        .lines()
-        .map(|line| line.split("|").filter_map(|s| s.parse().ok()).collect())
-        .collect();
-
-    let mut page_lists: Vec<Vec<i32>> = page_lists
-        .lines()
-        .map(|line| line.split(",").filter_map(|s| s.parse().ok()).collect())
-        .collect();
-
-    println!("Part 1: {}", solve_part1(&transitions, &page_lists));
-    println!(
-        "Part 2: {}",
-        solve_part2(&transitions, page_lists.as_mut_slice())
-    );
+#[derive(Debug)]
+struct TransitionRules {
+    allowed: TransitionMap,
+    forbidden: TransitionMap,
 }
 
-fn build_transition_maps(
-    transitions: &[Vec<i32>],
-) -> (HashMap<i32, HashSet<i32>>, HashMap<i32, HashSet<i32>>) {
-    let mut forward: HashMap<i32, HashSet<i32>> = HashMap::new();
-    let mut backward: HashMap<i32, HashSet<i32>> = HashMap::new();
+impl TransitionRules {
+    fn from_transitions(transitions: &[Vec<i32>]) -> Self {
+        let mut allowed: HashMap<i32, HashSet<i32>> = HashMap::new();
+        let mut forbidden: HashMap<i32, HashSet<i32>> = HashMap::new();
 
-    for transition in transitions {
-        let [before, after] = transition[..] else {
-            panic!("Invalid transition");
-        };
-        forward.entry(before).or_default().insert(after);
-        backward.entry(after).or_default().insert(before);
+        for transition in transitions {
+            let [from, to] = transition[..] else {
+                unreachable!("Transition should have 2 elements");
+            };
+            allowed.entry(from).or_default().insert(to);
+            forbidden.entry(to).or_default().insert(from);
+        }
+
+        Self { allowed, forbidden }
     }
 
-    (forward, backward)
-}
-
-fn invalid_page_list_indices(
-    page_lists: &[Vec<i32>],
-    good_transitions: &HashMap<i32, HashSet<i32>>,
-    bad_transitions: &HashMap<i32, HashSet<i32>>,
-) -> HashSet<usize> {
-    page_lists
-        .iter()
-        .enumerate()
-        .filter_map(|(i, page_list)| {
-            if !check_page_list_validity(page_list, good_transitions, bad_transitions) {
-                Some(i)
-            } else {
-                None
-            }
-        })
-        .collect()
-}
-
-fn check_page_list_validity(
-    page_list: &[i32],
-    good_transitions: &HashMap<i32, HashSet<i32>>,
-    bad_transitions: &HashMap<i32, HashSet<i32>>,
-) -> bool {
-    !page_list.iter().combinations(2).any(|v| {
-        let [a, b] = v[..] else { unreachable!() };
-
-        if let Some(bad_transitions_for_a) = bad_transitions.get(a) {
-            bad_transitions_for_a.contains(b)
-        } else if good_transitions.get(a).unwrap().contains(b) {
-            false
-        } else {
-            unreachable!("{} to {} not found", a, b)
+    fn is_valid_transition(&self, from: &i32, to: &i32) -> bool {
+        if self.forbidden.get(from).map_or(false, |f| f.contains(to)) {
+            return false;
         }
-    })
+
+        self.allowed.get(from).map_or_else(
+            || panic!("no transition exists from {} to {}", from, to),
+            |a| a.contains(to),
+        )
+    }
 }
 
-fn sum_page_list_centers(page_lists: &[Vec<i32>], pl_indices: &[usize]) -> i32 {
-    pl_indices.iter().fold(0, |acc, i| {
-        acc + page_lists[*i][(page_lists[*i].len() - 1) / 2]
-    })
+#[derive(Debug)]
+struct PageLists {
+    lists: Vec<PageList>,
 }
 
-fn solve_part1(transitions: &[Vec<i32>], page_lists: &[Vec<i32>]) -> i32 {
-    let (good_transitions, bad_transitions) = build_transition_maps(&transitions);
+impl PageLists {
+    fn from_str(input: &str) -> Self {
+        let lists = input
+            .lines()
+            .map(|line| line.split(',').filter_map(|s| s.parse().ok()).collect())
+            .collect();
+        Self { lists }
+    }
 
-    let invalid_pl_indices =
-        invalid_page_list_indices(page_lists, &good_transitions, &bad_transitions);
+    fn get_center_value(&self, index: usize) -> i32 {
+        let list = &self.lists[index];
+        list[list.len() / 2]
+    }
 
-    let valid_pl_indices = (0..page_lists.len())
-        .collect::<HashSet<usize>>()
-        .difference(&invalid_pl_indices)
-        .cloned()
-        .collect::<Vec<usize>>();
+    fn is_valid_list(&self, index: usize, rules: &TransitionRules) -> bool {
+        let list = &self.lists[index];
+        list.iter()
+            .tuple_combinations()
+            .all(|(a, b)| rules.is_valid_transition(a, b))
+    }
 
-    sum_page_list_centers(page_lists, &valid_pl_indices)
-}
+    fn find_invalid_indices(&self, rules: &TransitionRules) -> HashSet<usize> {
+        (0..self.lists.len())
+            .filter(|&i| !self.is_valid_list(i, rules))
+            .collect()
+    }
 
-fn solve_part2(transitions: &[Vec<i32>], page_lists: &mut [Vec<i32>]) -> i32 {
-    let (good_transitions, bad_transitions) = build_transition_maps(transitions);
+    fn sum_centers(&self, indices: &[usize]) -> i32 {
+        indices.iter().map(|&i| self.get_center_value(i)).sum()
+    }
 
-    let invalid_pl_indices =
-        invalid_page_list_indices(page_lists, &good_transitions, &bad_transitions);
-
-    for i in invalid_pl_indices.iter() {
-        let page_list = &mut page_lists[*i];
-
-        for v in (0..page_list.len()).combinations(2) {
-            let [j, k] = v[..] else { unreachable!() };
-
-            if let Some(bad_transitions_for_a) = bad_transitions.get(&page_list[j]) {
-                if bad_transitions_for_a.contains(&page_list[k]) {
-                    page_list.swap(j, k);
+    fn try_fix_invalid_lists(&mut self, rules: &TransitionRules) {
+        let invalid = self.find_invalid_indices(rules);
+        for &i in &invalid {
+            let list = &mut self.lists[i];
+            for (j, k) in (0..list.len()).tuple_combinations() {
+                if !rules.is_valid_transition(&list[j], &list[k]) {
+                    list.swap(j, k);
                 }
             }
         }
     }
+}
 
-    sum_page_list_centers(
-        page_lists,
-        &invalid_pl_indices.into_iter().collect::<Vec<_>>(),
-    )
+pub fn solve() {
+    let input = input::read_file("inputs/day05.txt");
+    let [transitions, page_lists] = input.split("\n\n").collect::<Vec<&str>>()[..] else {
+        panic!("Invalid input format");
+    };
+
+    let transitions: Vec<Vec<i32>> = transitions
+        .lines()
+        .map(|line| line.split('|').filter_map(|s| s.parse().ok()).collect())
+        .collect();
+
+    let mut page_lists = PageLists::from_str(page_lists);
+    let rules = TransitionRules::from_transitions(&transitions);
+
+    println!("Part 1: {}", solve_part1(&page_lists, &rules));
+    println!("Part 2: {}", solve_part2(&mut page_lists, &rules));
+}
+
+fn solve_part1(page_lists: &PageLists, rules: &TransitionRules) -> i32 {
+    let invalid = page_lists.find_invalid_indices(rules);
+    let valid: Vec<_> = (0..page_lists.lists.len())
+        .filter(|i| !invalid.contains(i))
+        .collect();
+    page_lists.sum_centers(&valid)
+}
+
+fn solve_part2(page_lists: &mut PageLists, rules: &TransitionRules) -> i32 {
+    let invalid: Vec<_> = page_lists.find_invalid_indices(rules).into_iter().collect();
+    page_lists.try_fix_invalid_lists(rules);
+    page_lists.sum_centers(&invalid)
 }
 
 #[cfg(test)]
@@ -131,13 +125,25 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_build_transition_maps() {
+    fn test_transition_rules() {
         let transitions = vec![vec![1, 2], vec![2, 3], vec![1, 3]];
-        let (forward, backward) = build_transition_maps(&transitions);
+        let rules = TransitionRules::from_transitions(&transitions);
 
-        assert_eq!(forward[&1], [2, 3].into_iter().collect());
-        assert_eq!(forward[&2], [3].into_iter().collect());
-        assert_eq!(backward[&2], [1].into_iter().collect());
-        assert_eq!(backward[&3], [1, 2].into_iter().collect());
+        assert!(rules.is_valid_transition(&1, &2));
+        assert!(rules.is_valid_transition(&1, &3));
+        assert!(rules.is_valid_transition(&2, &3));
+        assert!(!rules.is_valid_transition(&2, &1));
+        assert!(!rules.is_valid_transition(&3, &1));
+    }
+
+    #[test]
+    fn test_page_lists_validity() {
+        let lists = PageLists {
+            lists: vec![vec![1, 2, 3], vec![3, 2, 1]],
+        };
+        let rules = TransitionRules::from_transitions(&[vec![1, 2], vec![2, 3], vec![1, 3]]);
+
+        assert!(lists.is_valid_list(0, &rules));
+        assert!(!lists.is_valid_list(1, &rules));
     }
 }
